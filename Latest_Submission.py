2a3b8151fe469dbdbffc1458e59a9da5b775a044
Rajath John Bosco <jrajath94@gmail.com>
2020-01-03 16:08:20 -0600

    ++ rearranged Cells , fixed doctests

--- Latest_Submission.py
+++ Latest_Submission.py
@@ -27,10 +27,24 @@ def read_data(file, **kwargs):
     Traceback (most recent call last):
     ...
     NameError: name 'abcd' is not defined
-    >>> read_data('abcd.csv')
-    Traceback (most recent call last):
-    ...
-    FileNotFoundError: [Errno 2] File b'abcd.csv' does not exist: b'abcd.csv'
+    >>> read_data('Tests/olm2_test.csv')
+             Host City    Country Summer (Olympiad)  ...  Year   latitude   longitude
+    0      Albertville     France               NaN  ...  1992  45.675500    6.392700
+    1        Barcelona      Spain               XXV  ...  1992  41.388787    2.158985
+    2      Lillehammer     Norway               NaN  ...  1994  61.115300   10.466200
+    3          Atlanta        USA              XXVI  ...  1996  33.748995  -84.387985
+    4           Nagano      Japan               NaN  ...  1998  36.650000  138.183334
+    5           Sydney  Australia             XXVII  ...  2000 -33.867850  151.207321
+    6   Salt Lake City        USA               NaN  ...  2002  40.758701 -111.876183
+    7           Athens     Greece            XXVIII  ...  2004  37.983333   23.733334
+    8            Turin      Italy               NaN  ...  2006  45.070300    7.686900
+    9          Beijing      China              XXIX  ...  2008  39.907498  116.397224
+    10       Vancouver     Canada               NaN  ...  2010  49.249657 -123.119339
+    11          London         UK               XXX  ...  2012  51.508415   -0.125533
+    12           Sochi     Russia               NaN  ...  2014  43.600000   39.730278
+    13  Rio de Janeiro     Brazil              XXXI  ...  2016 -22.902778  -43.207501
+    <BLANKLINE>
+    [14 rows x 7 columns]
 
     >>> read_data('athlete_events.csv' , columns = 'asd')
     Traceback (most recent call last):
@@ -71,7 +85,7 @@ def read_data_df(file, **kwargs):
     13  Rio de Janeiro     Brazil              XXXI  ...  2016 -22.902778  -43.207501
     <BLANKLINE>
     [14 rows x 7 columns]
-    >>> read_data_df('athlete_events.csv' , columns = 'asd')
+    >>> read_data_df('Tests/olm2_test.csv' , columns = 'asd')
     Traceback (most recent call last):
     ...
     TypeError: parser_f() got an unexpected keyword argument 'columns'
@@ -271,4 +285,93 @@ def sprinter_stats(olympics, paramater):
         'All Medalists']
     # Athletics Men's 100 metres
     return sprint
+
+def FemaleTrend(olympics_NOC,Sex,Season):
+    """
+    In this function, the rise in the female trend is analyzed over the summer and the winter season
+
+    >>> FemaleTrend('unknown.dataframe', 'Sex')
+    Traceback (most recent call last):
+    ...
+    TypeError: FemaleTrend() missing 1 required positional argument: 'Season'
+
+    >>> FemaleTrend(df, unknown_argument)
+    Traceback (most recent call last):
+    ...
+    NameError: name 'df' is not defined
+
+
+    :param olympics_NOC:
+    :return:
+    """
+    Femalecount = olympics_NOC[(olympics_NOC['Sex'] == Sex) & (olympics_NOC['Season'] == Season) ].groupby(['Year'])['Name'].count().reset_index(name="Female Trend")
+    print(Femalecount)
+    Femalecount.plot(kind='line',x='Year',y='Female Trend')
+    
+
+def events(olympics_NOC,Season):
+    """
+    In this function, the count of all different events is displayed in bar graph over the Summer and Winter Olympics
+    
+    >>> events_summer('unknown.dataframe', 'country','host')
+    Traceback (most recent call last):
+    ...
+    NameError: name 'events_summer' is not defined
+
+    >>> events_summer(df, unknown_argument)
+    Traceback (most recent call last):
+    ...
+    NameError: name 'events_summer' is not defined
+
+
+    :param olympics_NOC_gdp:
+    :return:
+    """
+#Plotting Event Count over the years in Summer Olympic
+
+    Eventcount = olympics_NOC[olympics_NOC['Season'] == Season].groupby(['Year'])['Event'].nunique().reset_index(name="Eventcount")
+    print(Eventcount)
+    fig,ax = plt.subplots(figsize=(28,6))
+    Eventcount.plot(kind='bar',x='Year',y='Eventcount',ax=ax,color='red')
+
+    for p in ax.patches:
+        ax.text(p.get_x() + p.get_width()/2., p.get_height(), '%d' % int(p.get_height()), 
+                fontsize=12, color='black', ha='center', va='bottom')
+
+
+def athletes(olympics_NOC,Season):
+    """
+    In this function, the count of all athlete participation is displayed in bar graph over the Summer and Winter Olympics
+    
+    >>> athletes()
+    Traceback (most recent call last):
+    ...
+    TypeError: athletes() missing 2 required positional arguments: 'olympics_NOC' and 'Season'
+
+    >>> athletes('unknown.dataframe')
+    Traceback (most recent call last):
+    ...
+    TypeError: athletes() missing 1 required positional argument: 'Season'
+
+    >>> athletes(PandasDF)
+    Traceback (most recent call last):
+    ...
+    NameError: name 'PandasDF' is not defined
+
+
+    :param olympics_NOC:
+    :return:
+    """
+
+    #Plotting Athlete count over the years in Summer Olympic
+
+    Athletecount = olympics_NOC[olympics_NOC['Season'] == Season].groupby(['Year'])['Name'].nunique().reset_index(name="Athletecount")
+    print(Athletecount)
+    fig,ax = plt.subplots(figsize=(22,6))
+    Athletecount.plot(kind='bar',x='Year',y='Athletecount',ax=ax,color='red')
+
+    for p in ax.patches:
+        ax.text(p.get_x() + p.get_width()/2., p.get_height(), '%d' % int(p.get_height()), 
+                fontsize=12, color='black', ha='center', va='bottom')
+
 
